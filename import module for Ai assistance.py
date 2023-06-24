c43b1d4dc66c0be26928f120ebb71bac74cb7498
NisanthSakthivel <137193351+NisanthSakthivel@users.noreply.github.com>
2023-06-24 20:43:04 +0530

    Add files via upload

---
+++ import module for Ai assistance.py
@@ -0,0 +1,14 @@
+# pip install pyttsx3          
+# pip insta ll speechRecognition     
+# pip install datetime               
+# pip install wikipedia              
+# pip install webbrowser            
+# pip install os                    
+# pip install smtplib                 
+# pip install times                  
+# pip install ctypes                
+# pip install subprocess        
+# pip install winshell                       
+# pip install translator                
+# pip install ecapture as ac
+                             
